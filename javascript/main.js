c718ad8938d514c26eb495ed4259539159cd042e
Tasha <shradertasha@gmail.com>
2019-03-08 11:25:54 -0600

    updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,4 @@
-$("#gify").on('click', function () {
-    window.location = "https://tashashrader.github.io/GifTastic/";
-});
 
-$("#crystal").on('click', function () {
-    window.location = "https://tashashrader.github.io/unit-4-game/";
-})
 
 $("#wireframe").on('click', function () {
     window.location = "https://tashashrader.github.io/HW-Wireframe/";
@@ -12,4 +6,24 @@ $("#wireframe").on('click', function () {
 
 $("#scratch").on('click', function () {
     window.location = "https://github.com/minstp201901fsf3-ft/ScratchMatch";
+})
+$("#crystal").on('click', function () {
+    window.location = "https://tashashrader.github.io/unit-4-game/";
+})
+$("#gify").on('click', function () {
+    window.location = "https://tashashrader.github.io/GifTastic/";
+});
+
+
+$("#movie").on('click', function() {
+    window.location = "https://tashashrader.github.io/MovieSurvey";
+})
+$('#liri').on('click', function(){
+  window.location= "https://github.com/TashaShrader/liri-node-app";
+})
+$('#bamazon').on('click', function(){
+    window.location= "https://github.com/TashaShrader/bamazon";
+    })
+$('#friend-finder').on('click', function(){
+window.location= "https://friendfinder019.herokuapp.com/";
 })
